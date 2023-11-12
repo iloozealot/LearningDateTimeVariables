261dee5295e170b0811660e2923f16088fe29578
iloozealot <iloozealot@gmail.com>
2023-11-12 14:24:44 -0600

    Add project files.

---
+++ LearningDateTimeVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Globalization;
+
+//DateTime today = DateTime.Now;
+
+DateTime today = DateTime.UtcNow;
+
+//DateTime birthday = DateTime.Parse("6/15/1998");
+//DateTime birthday = DateTime.ParseExact("6/11/1998", "d/M/yyyy", CultureInfo.InvariantCulture);
+
+
+Console.WriteLine(today.ToString("MMMM dd, yyyy hh:mm tt zzz"));
+//Console.WriteLine(birthday.ToString());
